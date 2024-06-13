c78ae2bdcbecdffd3cf40e14dfadadb9edbeb8d1
teira <inateira@gmail.com>
2024-06-13 12:53:50 -0300

    Modificados TDA Lista y Nodo para que funcionen sin .cpp (son templates)

--- TP2/src/Nodo.h
+++ TP2/src/Nodo.h
@@ -53,4 +53,29 @@ public:
 	void setSiguiente(Nodo<T>* siguiente);
 };
 
+template <typename T> Nodo<T>::Nodo(T dato) {
+	this->dato = dato;
+	this->siguiente = NULL;
+}
+
+template <typename T> Nodo<T>::~Nodo() {}
+
+
+template <typename T> T Nodo<T>::getDato() {
+	return this->dato;
+}
+
+template <typename T> void Nodo<T>::setDato(T dato) {
+	this->dato = dato;
+}
+
+template <typename T> Nodo<T>* Nodo<T>::getSiguiente() {
+	return this->siguiente;
+}
+
+template <typename T> void Nodo<T>::setSiguiente(Nodo<T>* siguiente) {
+	this->siguiente = siguiente;
+}
+
+
 #endif /* NODO_H_ */
